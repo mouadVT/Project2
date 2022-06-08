318307930619206e587323e0a61e7f041ba93152
1alim <1alim@DESKTOP-N3GIPR9>
2022-06-08 17:58:38 -0400

    new classes internal/fw

---
+++ CS5040P2/src/Internalnode.java
@@ -0,0 +1,31 @@
+
+public class Internalnode implements Baseclass {
+
+    @Override
+    public void insert() {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public void print() {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public void search() {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public void remove() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ CS5040P2/src/flyweightnode.java
@@ -0,0 +1,31 @@
+
+public class flyweightnode implements Baseclass {
+
+    @Override
+    public void insert() {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public void print() {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public void search() {
+        // TODO Auto-generated method stub
+
+    }
+
+
+    @Override
+    public void remove() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
