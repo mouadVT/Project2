ab0acd0276026c26fd9ae4b60d4af0fed7a578ce
1alim <1alim@DESKTOP-N3GIPR9>
2022-06-07 21:08:36 -0400

    These are the classes we have so far!

---
+++ CS5040P2/src/DNAtree2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+
+/**
+ * @author 1alim
+ *
+ */
+public class DNAtree2 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ CS5040P2/src/EmptyLeafNode.java
@@ -0,0 +1,48 @@
+
+public class EmptyLeafNode implements Node {
+
+    private static EmptyLeafNode flyweight = null; // Single flyweight for all
+                                                    // empty leaf nodes
+
+    /**
+     * Factory method to access flyweight
+     * 
+     * @return
+     */
+    public static EmptyLeafNode getInstance() {
+        if (flyweight == null) {
+            flyweight = new EmptyLeafNode();
+        }
+        return flyweight;
+    }
+
+    /**
+     * Constructs a Flyweight Private constructor for Singleton pattern. Can
+     * only be instantiated internally
+     * 
+     * @see EmptyLeafNode#getInstance()
+     */
+    private EmptyLeafNode() {
+        // empty constructor
+    }
+
+    /**
+     * Print representation of empty leaf node
+     */
+    @Override
+    public void print() {   }
+
+    /**
+     * Only called when an empty leaf-node. Replace self with new non-empty leaf
+     * node (SequenceNode)
+     * 
+     * @return the new SequenceNode that will replace this Node in the tree
+     */
+    @Override
+    public Node insert(SavedSequence sequence) {
+        return new LeafNode(sequence);
+    }
+
+    /**
+     * Attempting to remove an empty leaf node means that we did not find
+
