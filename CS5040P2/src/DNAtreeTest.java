9fef9159c461e6c870969aed3ffae5f73c16f2bf
1alim <1alim@DESKTOP-N3GIPR9>
2022-06-13 21:38:38 -0400

    Created test classes

---
+++ CS5040P2/src/DNAtreeTest.java
@@ -0,0 +1,15 @@
+
+import student.TestCase;
+
+public class DNAtreeTest extends TestCase  {
+    private DNAtree tree ;
+
+    /**
+     * Sets up each test method.
+     */
+    public void setUp() {
+        tree = new DNAtree();
+    }
+    
+    
+}
---
+++ CS5040P2/src/InternalnodeTest.java
@@ -0,0 +1,13 @@
+
+import student.TestCase;
+
+public class InternalnodeTest extends TestCase  {
+    private Internalnode intNode;
+
+    /**
+     * Sets up each test method.
+     */
+    public void setUp() {
+        intNode= new Internalnode();
+    }
+}
