9575649fb4fbf1fac07204ae46f4aa479a180d68
James <James@DESKTOP-F9IODE8>
2022-06-16 22:12:43 -0400

    baseclass + internalnode style fixed

--- CS5040P2/src/Baseclass.java
+++ CS5040P2/src/Baseclass.java
@@ -1,27 +1,53 @@
 /**
  * DNA tree node abstract class that defines operations for nodes.
+ * 
+ * @author James Jang (jangwool)
+ * @author Mouad Ait Taleb Ali (mouad)
+ * @version 06/08/2022
  */
 public abstract class Baseclass {
     /**
      * Inserts sequence into tree node.
+     * 
+     * @param entry
+     *            (String of DNA sequence)
+     * @param depth
+     *            (depth/level of node)
+     * @return node type after insertion
      */
     public abstract Baseclass insert(String entry, int depth);
 
 
     /**
      * Prints out DNA tree.
+     * 
+     * @param depth
+     *            (depth/level of node)
+     * @param entry
+     *            (String of DNA sequence)
      */
     public abstract void print(int depth, String entry);
 
 
     /**
      * Finds all sequences that match the given sequence.
+     * 
+     * @param entry
+     *            (String of DNA sequence)
+     * @param depth
+     *            (depth/level of node)
      */
     public abstract void search(String entry, int depth);
 
 
     /**
      * Removes a sequence from the tree.
+     * 
+     * @param entry
+     *            (String of DNA sequence)
+     * @param depth
+     *            (depth/level of node)
+     * @return removed node
      */
     public abstract Baseclass remove(String entry, int depth);
 
@@ -30,7 +56,9 @@ public abstract class Baseclass {
      * Returns number of nodes visited.
      * 
      * @param entry
+     *            (String of DNA sequence)
      * @param depth
+     *            (depth/level of node)
      * @return integer
      */
     public abstract int numOfNodeVisited(String entry, int depth);
--- CS5040P2/src/Internalnode.java
+++ CS5040P2/src/Internalnode.java
@@ -1,17 +1,37 @@
+/**
+ * Internalnode class.
+ * 
+ * @author James Jang (jangwool)
+ * @author Mouad Ait Taleb Ali (mouad)
+ * @version 06/08/2022
+ */
 public class Internalnode extends Baseclass {
 
-    private Baseclass a, c, g, t, $;
+    private Baseclass a;
+    private Baseclass c;
+    private Baseclass g;
+    private Baseclass t;
+    private Baseclass dollar;
 
+    /**
+     * Default constructor.
+     */
     public Internalnode() {
         this.a = new flyweightnode();
         this.c = new flyweightnode();
         this.g = new flyweightnode();
         this.t = new flyweightnode();
-        this.$ = new flyweightnode();
+        this.dollar = new flyweightnode();
     }
 
 
-    // used for testing purposes
+    /**
+     * Returns child node.
+     * 
+     * @param dna
+     *            (char)
+     * @return child node
+     */
     public Baseclass getChild(char dna) {
         if (dna == 'A') {
             return a;
@@ -26,12 +46,17 @@ public class Internalnode extends Baseclass {
             return t;
         }
         else if (dna == '$') {
-            return $;
+            return dollar;
         }
-        return $;
+        return dollar;
     }
 
 
+    /**
+     * Returns child if it is a leaf node.
+     * 
+     * @return leaf node
+     */
     public Baseclass getLeaf() {
         if (a instanceof LeafNode) {
             return a;
@@ -49,30 +74,17 @@ public class Internalnode extends Baseclass {
     }
 
 
+    /**
+     * Inserts sequence into internal node.
+     * 
+     * @param entry
+     *            (String of DNA sequence)
+     * @param depth
+     *            (depth/level of node)
+     * @return internal node
+     */
     @Override
     public Baseclass insert(String entry, int depth) {
-        /*
-         * if (depth < entry.length()) {
-         * if (entry.charAt(depth) == 'A') {
-         * a = a.insert(entry, depth+1);
-         * }
-         * else if (entry.charAt(depth) == 'C') {
-         * c = c.insert(entry, depth+1);
-         * }
-         * else if (entry.charAt(depth) == 'G') {
-         * g = g.insert(entry, depth+1);
-         * }
-         * else if (entry.charAt(depth) == 'T') {
-         * t = t.insert(entry, depth+1);
-         * }
-         * else if (entry.charAt(depth) == '$') {
-         * $ = $.insert(entry, depth+1);
-         * }
-         * return this;
-         * }
-         * return this;
-         */
-
         if (depth < entry.length()) {
             if (entry.charAt(depth) == 'A') {
                 a = a.insert(entry, depth + 1);
@@ -88,11 +100,19 @@ public class Internalnode extends Baseclass {
             }
             return this;
         }
-        $ = $.insert(entry, depth + 1);
+        dollar = dollar.insert(entry, depth + 1);
         return this;
     }
 
 
+    /**
+     * Prints out "I" for internal node.
+     * 
+     * @param depth
+     *            (depth/level of node)
+     * @param entry
+     *            (String of DNA sequence)
+     */
     @Override
     public void print(int depth, String entry) {
         String st0 = "";
@@ -105,10 +125,18 @@ public class Internalnode extends Baseclass {
         c.print(depth + 1, entry);
         g.print(depth + 1, entry);
         t.print(depth + 1, entry);
-        $.print(depth + 1, entry);
+        dollar.print(depth + 1, entry);
     }
 
 
+    /**
+     * Finds all sequences that match the given sequence.
+     * 
+     * @param entry
+     *            (String of DNA sequence)
+     * @param depth
+     *            (depth/level of node)
+     */
     @Override
     public void search(String entry, int depth) {
         if (depth < entry.length()) {
@@ -125,7 +153,7 @@ public class Internalnode extends Baseclass {
                 t.search(entry, depth + 1);
             }
             else {
-                $.search(entry, depth);
+                dollar.search(entry, depth);
             }
         }
         else {
@@ -141,13 +169,22 @@ public class Internalnode extends Baseclass {
             if (!(t instanceof flyweightnode)) {
                 t.search(entry, depth + 1);
             }
-            if (!($ instanceof flyweightnode)) {
-                $.search(entry, depth + 1);
+            if (!(dollar instanceof flyweightnode)) {
+                dollar.search(entry, depth + 1);
             }
         }
     }
 
 
+    /**
+     * Removes a sequence from the tree.
+     * 
+     * @param entry
+     *            (String of DNA sequence)
+     * @param depth
+     *            (depth/level of node)
+     * @return removed node
+     */
     @Override
     public Baseclass remove(String entry, int depth) {
         if (depth < entry.length()) {
@@ -165,11 +202,21 @@ public class Internalnode extends Baseclass {
             }
             return this.returnLeafNode();
         }
-        $ = $.remove(entry, depth);
+        dollar = dollar.remove(entry, depth);
         return this.returnLeafNode();
     }
 
 
+    /**
+     * Sets data of leaf node.
+     * 
+     * @param sequence
+     *            (String of DNA sequence)
+     * @param depth
+     *            (depth/level of node)
+     * @param index
+     *            (char)
+     */
     public void setTheLeafNode(String sequence, int depth, char index) {
 
         switch (index) {
@@ -186,12 +233,21 @@ public class Internalnode extends Baseclass {
                 this.t = new LeafNode(sequence, depth);
                 break;
             default:
-                this.$ = new LeafNode(sequence, depth);
+                this.dollar = new LeafNode(sequence, depth);
                 break;
         }
     }
 
 
+    /**
+     * Counts number of nodes visited.
+     * 
+     * @param entry
+     *            (String of DNA sequence)
+     * @param depth
+     *            (depth/level of node)
+     * @return total number of visited nodes
+     */
     public int numOfNodeVisited(String entry, int depth) {
         if (depth < entry.length()) {
             char character = entry.charAt(depth);
@@ -205,63 +261,76 @@ public class Internalnode extends Baseclass {
                 case 'T':
                     return 1 + t.numOfNodeVisited(entry, depth + 1);
                 default:
-                    return 1 + $.numOfNodeVisited(entry, depth + 1);
+                    return 1 + dollar.numOfNodeVisited(entry, depth + 1);
             }
         }
         return 1 + a.numOfNodeVisited(entry, depth + 1) + c.numOfNodeVisited(
             entry, depth + 1) + g.numOfNodeVisited(entry, depth + 1) + t
-                .numOfNodeVisited(entry, depth + 1) + $.numOfNodeVisited(entry,
-                    depth + 1);
+                .numOfNodeVisited(entry, depth + 1) + dollar.numOfNodeVisited(
+                    entry, depth + 1);
     }
 
 
+    /**
+     * Returns leaf node.
+     * 
+     * @return leaf node
+     */
     private Baseclass returnLeafNode() {
         boolean aLeaf = a instanceof LeafNode;
         boolean cLeaf = c instanceof LeafNode;
         boolean gLeaf = g instanceof LeafNode;
         boolean tLeaf = t instanceof LeafNode;
-        boolean $Leaf = $ instanceof LeafNode;
-        if (this.numOfFlyNodes() == 4) {
-            if (this.numOfLeafNodes() == 1) {
-                if (aLeaf)
-                    return a;
-                if (cLeaf)
-                    return c;
-                if (gLeaf)
-                    return g;
-                if (tLeaf)
-                    return t;
-                if ($Leaf)
-                    return $;
+        boolean dollarLeaf = dollar instanceof LeafNode;
+        if (this.numOfFlyNodes() == 4 && this.numOfLeafNodes() == 1) {
+            if (aLeaf) {
+                return a;
+            }
+            if (cLeaf) {
+                return c;
+            }
+            if (gLeaf) {
+                return g;
+            }
+            if (tLeaf) {
+                return t;
+            }
+            if (dollarLeaf) {
+                return dollar;
             }
         }
         return this;
-
     }
 
 
+    /**
+     * Counts number of flyweight nodes.
+     * 
+     * @return number of flyweight nodes
+     */
     private int numOfFlyNodes() {
         flyweightnode flw = new flyweightnode();
         return Boolean.compare(a.equals(flw), false) + Boolean.compare(c.equals(
             flw), false) + Boolean.compare(g.equals(flw), false) + Boolean
-                .compare(t.equals(flw), false) + Boolean.compare($.equals(flw),
-                    false);
+                .compare(t.equals(flw), false) + Boolean.compare(dollar.equals(
+                    flw), false);
     }
 
 
+    /**
+     * Counts number of leaf nodes.
+     * 
+     * @return number of leaf nodes
+     */
     private int numOfLeafNodes() {
         boolean aLeaf = a instanceof LeafNode;
         boolean cLeaf = c instanceof LeafNode;
         boolean gLeaf = g instanceof LeafNode;
         boolean tLeaf = t instanceof LeafNode;
-        boolean $Leaf = $ instanceof LeafNode;
+        boolean dollarLeaf = dollar instanceof LeafNode;
 
         return Boolean.compare(aLeaf, false) + Boolean.compare(cLeaf, false)
             + Boolean.compare(gLeaf, false) + Boolean.compare(tLeaf, false)
-            + Boolean.compare($Leaf, false);
+            + Boolean.compare(dollarLeaf, false);
     }
-
-    // do we have to implement equals or comparison method at all?
-
-    //
 }
